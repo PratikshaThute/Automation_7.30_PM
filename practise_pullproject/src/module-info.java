baef3b1fc92099a912e7f357e018a64def55a507
PratikshaThute <155456788+PratikshaThute@users.noreply.github.com>
2024-06-17 18:35:33 +0530

    Added java project

---
+++ practise_pullproject/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module practise_pullproject {
+}
---
+++ practise_pullproject/src/practise_pullproject1/Practise.java
@@ -0,0 +1,15 @@
+package practise_pullproject1;
+
+public class Practise {
+	
+	public void name() {
+		System.out.println("To pull project");
+	}
+
+	public static void main(String[] args) {
+		Practise p = new Practise();
+		p.name();
+
+	}
+
+}
